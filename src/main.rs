3e7a5eb84248d64e76353954b4bac357efe92763
WoofyJack <me@domain>
2023-06-24 13:24:11 +0800

    Now actually prints something onscreen

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 #![allow(dead_code)]
 use rand::Rng;
-
+use std::ops::Add;
+use std::fmt;
 use std::collections::HashMap;
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Debug,Copy,Clone,Eq, Hash, PartialEq)]
 struct Coordinate(i8, i8);
+impl Add for Coordinate {
+    type Output = Self;
+    fn add(self, other:Self) -> Self{
+        Coordinate(self.0+other.0, self.1+other.1)
+    }
+}
+impl fmt::Display for Coordinate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{},{}]", self.0, self.1)
+    }
+}
 
 #[derive(Debug)]
 struct Delver {
@@ -19,6 +31,11 @@ impl Delver {
         Delver {name, exploriness:0.5, fightiness:0.5, hp:5}
     }
 }
+impl fmt::Display for Delver {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
 
 
 enum GamePhase {
@@ -31,9 +48,14 @@ enum GamePhase {
 }
 
 struct Room {
-    coord:Coordinate,
     complete:bool
 }
+impl Room {
+    fn new_room() -> Room {
+        Room {complete: false}
+    }
+}
+
 struct Game {
     phase:GamePhase,
     delvers:Vec<Delver>,
@@ -55,29 +77,31 @@ impl Game {
 fn main() {
     let mut rng = rand::thread_rng();
 
-    roll(&mut rng);
-
     let delvers = Vec::new();
-    let rooms = HashMap::new();
+    let rooms: HashMap<Coordinate, Room> = HashMap::new();
     let mut game_state = Game::new_game(delvers, rooms);
     
     let c = Delver::new_delver(String::from("Rogue"));
-
     game_state.delvers.push(c);
-
     let c = Delver::new_delver(String::from("Fighter"));
     game_state.delvers.push(c);
 
+    game_state.rooms.insert(Coordinate(0,0), Room::new_room());
+    game_state.rooms.insert(Coordinate(1,0), Room::new_room());
+    game_state.rooms.insert(Coordinate(2,0), Room::new_room());
+
+    for _ in 0..10 {
+    tick(&mut game_state, &mut rng);
+    }
 }
 
-fn roll(rng: &mut impl Rng) {
-    let result:f32 = rng.gen();
-    println!("{}",result);
+fn roll(rng: &mut impl Rng) -> f32 {
+    rng.gen()
 }
 
-fn tick(game:&mut Game) {
+fn tick(game:&mut Game, rng:&mut impl Rng) -> () {
     let active_delver = &game.delvers[game.delver_index as usize];
-    let current_room = &game.rooms.get(&game.delver_position).expect("Handle Error later");
+    let mut current_room: &mut Room = game.rooms.get_mut(&game.delver_position).expect("Handle Error later");
 
     match game.phase {
         GamePhase::NotStarted => {
@@ -86,18 +110,29 @@ fn tick(game:&mut Game) {
         GamePhase::TurnStart => {
             game.phase = GamePhase::Encounter;
             game.delver_index += 1;
-            if game.delver_index >= game.delvers.len().try_into().expect("Impossibly many delvers. Handle later.") {game.delver_index = 0;}
+            if game.delver_index >= game.delvers.len().try_into()// Converts len() into a u8. Shouldn't be possible to get 256 delvers, but should add in error handling.
+            .expect("Impossibly many delvers. Handle later.") {game.delver_index = 0;}
             //active_delver and current_room not currently valid.
         }
         GamePhase::Encounter => {
             if current_room.complete {game.phase = GamePhase::Forge;}
             else {
                 game.phase = GamePhase::TurnStart;
-
+                // Do encounter rolls
+                current_room.complete = true;
+                println!("{}", active_delver);
             }
         }
-        GamePhase::Forge => {}
-        GamePhase::Travel => {}
+        GamePhase::Forge => {
+            game.phase = GamePhase::Travel;
+            //Do forging stuff
+        }
+        GamePhase::Travel => {
+            game.phase = GamePhase::TurnStart;
+            // Do Travel stuff
+            game.delver_position = game.delver_position + Coordinate(1,0);
+            println!("{}",game.delver_position);
+        }
         GamePhase::Finished => {}
     }
 }
